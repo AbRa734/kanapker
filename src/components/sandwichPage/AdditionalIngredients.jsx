14db6e0cdaa511edb1f760b7993c016980dd71d1
AbRa734 <sz.o@yahoo.com>
2024-03-13 17:41:08 +0100

    start

---
+++ src/components/sandwichPage/AdditionalIngredients.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AdditionalIngredients(props) {
+    return (
+        <div>
+            <div>Łączna cena: 30.00zł</div>
+            <button>Dodaj do koszyka</button>
+        </div>
+    );
+}
+
+export default AdditionalIngredients;
